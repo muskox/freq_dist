77a6b4ef7ce86c11a9117e33d43e095385875f06
Scott Pustay <scott.pustay@gmail.com>
2012-06-01 19:12:43 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 freq_dist
 =========
+
+I am reading Mining the Social Web and I modified the example for Frequency Distribution for twitter searches. Input search terms to see their Frequency Distribution attributes.
+
+The hardest part about using this code is having the proper python libraries installed. I used easy_install for all of them.
+
---
+++ freq_dist.py
@@ -0,0 +1,54 @@
+
+import twitter
+#import json  # -> Not using this unless you want to print out strings
+import sys
+import nltk
+
+if len(sys.argv) < 2:
+    print 'Please add search terms to this script '
+    print 'for example, $python freq_dist.py lady gaga'
+    sys.exit()
+
+search_str = ''
+counter = 0
+for parts in sys.argv:
+    if counter == 0:
+        pass
+    else:
+        search_str += parts + ' '
+    counter = counter + 1
+
+
+twitter_search = twitter.Twitter(domain="search.twitter.com")
+search_results = []
+for page in range(1,6):
+    search_results.append(twitter_search.search(q=search_str, rpp=100, page=page))
+
+#print json.dumps(search_results, sort_keys=True, indent=1)
+
+tweets = [ r['text'] \
+    for result in search_results \
+        for r in result['results'] ]
+words = []
+for t in tweets:
+    words += [ w for w in t.split() ]
+
+print 'total words: ' + str(len(words))
+if len(words) == 0:
+    sys.exit()
+
+print 'unique words: ' + str(len(set(words))) 
+
+print 'lexical diversity: ' + str(1.0*len(set(words))/len(words))
+
+print 'avg words per tweet: ' + str(1.0*sum([ len(t.split()) for t in tweets ])/len(tweets))
+
+freq_dist = nltk.FreqDist(words)
+
+print '50 most frequent tokens: '
+str_of_toks = ''
+for tok in freq_dist.keys()[:50]:
+    str_of_toks += tok + ' '
+
+print str_of_toks
+
